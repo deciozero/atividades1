44be9f6d8a3fc20c2939787f987492cb825769b8
Edecio <edeciogomes@globomail.com>
2018-04-21 15:33:16 -0300

    Atiividades1

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace DiaSemana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int diaSemana;
+            string nome;
+            Console.WriteLine("Digite o dia da semana, de 1 a 7:");
+            diaSemana = int.Parse(Console.ReadLine());
+            if (diaSemana == 1)
+            {
+            nome = "Domingo";
+            }
+                else if (diaSemana == 2)
+                {
+                    nome = "Segunda-feira";
+                }
+                else if (diaSemana == 3)
+                {
+                    nome ="Terça feira";
+                }
+                else if (diaSemana == 4)
+                {
+                    nome = "Quarta-feira";
+
+                }   
+                else if (diaSemana == 5)
+                {
+                    nome = "Quinta-feira";
+                }     
+               else if (diaSemana == 6)
+                {
+                    nome = "Sexta-feria";
+                }
+                else if (diaSemana == 7)
+                {
+                    nome = "Sábado";
+                }
+                else
+                {
+                    nome = "Dia inválido";
+
+                }
+                Console.WriteLine("O dia escolhido foi {0}", nome);
+                Console.ReadLine();
+
+            }
+            }                  
+        }
+
